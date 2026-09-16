c3fa28d08d4985db8c2e368a1657afee58aba43b
ArunkumarKathiravan <ArunkumarKathiravan@users.noreply.github.com>
2026-09-16 23:53:39 +0530

    Set up backend template styles

--- django_react_proj/django_react_proj/settings.py
+++ django_react_proj/django_react_proj/settings.py
@@ -80,7 +80,7 @@ ROOT_URLCONF = 'django_react_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -144,3 +144,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
