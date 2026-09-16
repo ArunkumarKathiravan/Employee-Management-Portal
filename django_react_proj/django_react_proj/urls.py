e82285f63dd3d56b6d5d3434041e491747c70e39
ArunkumarKathiravan <ArunkumarKathiravan@users.noreply.github.com>
2026-09-16 23:57:39 +0530

    Add a backend home route

--- django_react_proj/django_react_proj/urls.py
+++ django_react_proj/django_react_proj/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
+from django.views.generic import TemplateView
 from employees import views
 from rest_framework_jwt.views import obtain_jwt_token
 
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('token-auth/', obtain_jwt_token),
     re_path(r'^api/employees/$', views.employees_list),
